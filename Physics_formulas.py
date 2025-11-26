86b4978a8aef249d68feb14459239d28f8856ee9
unknown <samuelehalaiye@gmail.com>
2025-11-26 14:36:20 +0100

    modified my physics formula and variables

---
+++ Physics_formulas.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age: "))
+height = float(input("Enter your height: "))
+weight = 70
+if age >= 18 or height >= 1.7:
+    print("you are eligible")
+else:
+    print("you are not eligible")
---
+++ variables.py
@@ -0,0 +1,5 @@
+age = 18
+height = 1.75
+weight = 70
+if age < 18 or height < 1.75 or weight < 70:
+    print("you are eligible")
